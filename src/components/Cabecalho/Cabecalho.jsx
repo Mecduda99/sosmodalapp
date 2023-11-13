50f9002fedd47bd399ef52db0d9e607f8122acd3
mecduda99 <rm98760@fiap.com.br>
2023-11-13 01:52:00 -0300

    cabecalho com links sem rota

---
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -0,0 +1,23 @@
+"use client";
+import Link from 'next/link';
+import "./Cabecalho.css";
+import { useState } from 'react';
+
+export default function Cabecalho() {
+    return(
+    <header className='cabecalho'>
+        <nav>   
+            <Link href="/">LOGOUT</Link>
+            <div className='menu-sections'>
+                <Link className='link' href="/produtos/calca">HOME</Link>
+                <Link className='link' href="/produtos/camisa">SOBRE NOS</Link>
+                <Link className='link' href="/produtos/meia">FALE CONOSCO</Link>
+            </div>
+
+            <div>
+                <p>usuario - email</p>
+            </div>
+        </nav>
+    </header>       
+    )
+}
